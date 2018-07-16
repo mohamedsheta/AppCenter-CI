c140dc1337fcd89a2e5c77a3c8a143adc7ce49e5
Mohamed Sheta <mohamed.sheta@itworx.com>
2018-07-16 16:45:19 +0200

    initial commit

---
+++ TestAppForCI/TestAppForCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppForCI
+//
+//  Created by Mohamed Sheta on 7/16/18.
+//  Copyright © 2018 Mohamed Sheta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
